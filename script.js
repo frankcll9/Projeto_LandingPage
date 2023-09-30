3aaa1dca0b0265c98cc436cf0164aceb8aeaed94
Frank <121838262+frankcll9@users.noreply.github.com>
2023-09-30 14:17:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Adicione seu código JavaScript aqui, se necessário
+document.getElementById('signup-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Você se inscreveu com sucesso!');
+});
